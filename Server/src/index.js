3c6cc5ab7c3777b6935982fb529fbcb0f8f5c00a
gabino mendez <gabino_mendez@hotmail.com.ar>
2023-04-26 15:05:36 -0300

    agregando tests

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,24 +1,5 @@
-const express = require("express");
-const server = express();
+const server = require("./app");
 const PORT = 3001;
-const morgan = require("morgan");
-const {router} = require("../src/routes/index");
-
-server.use(morgan("dev"));
-server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
-server.use(express.json());
-
-
-server.use('/rickandmorty', router);
 
 server.listen(PORT, () => {
   console.log("server listen on port: " + PORT);
